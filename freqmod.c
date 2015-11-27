f6ac2452cec088cd187494c614ab68f6461cb295
Colin <colin.diesh@gmail.com>
2015-11-27 14:14:03 -0600

    Remove inner loop

--- freqmod.c
+++ freqmod.c
@@ -24,32 +24,37 @@ int main() {
     float theta_c=0;
     float I_t=0;
     int sign=1;
+    int index=0;
     char ch;
     WINDOW *w = initscr();
     cbreak();
     nodelay(w, TRUE);
     noecho();
     while(1) {
-        for(int i=0;i<B_SIZE;i++) {
-            if((ch=getch())!=-1) {
-                if(ch=='p') { endwin(); return 0; }
-                else if(ch=='q') { f_c*=2; }
-                else if(ch=='w') { f_c/=2; }
-                else if(ch=='a') { f_m*=2; }
-                else if(ch=='s') { f_m/=2; }
-                else if(ch=='z') { theta_m+=.01; }
-                else if(ch=='x') { theta_m-=.01; }
-                else if(ch=='c') { theta_c+=.01; }
-                else if(ch=='v') { theta_c-=.01; }
-            }
-            p1 += f_c*2*M_PI/S_RATE+theta_c;
-            p2 += f_m*2*M_PI/S_RATE+theta_m;
-            I_t += 0.0001*sign;
-            if(I_t>100||I_t<0) sign=-sign;
-
-            buffer[i]=(int) (amplitude*(cos(p1+I_t*(cos(p2)))));
+        if((ch=getch())!=-1) {
+            if(ch=='p') { break; }
+            else if(ch=='q') { f_c*=2; }
+            else if(ch=='w') { f_c/=2; }
+            else if(ch=='a') { f_m*=2; }
+            else if(ch=='s') { f_m/=2; }
+            else if(ch=='z') { theta_m+=.01; }
+            else if(ch=='x') { theta_m-=.01; }
+            else if(ch=='c') { theta_c+=.01; }
+            else if(ch=='v') { theta_c-=.01; }
         }
-        fprintf(stderr,"f_c=%02.2f f_m=%02.2f theta_c=%02.2f theta_m=%02.2f I_t=%02.2f\r",f_c,f_m,theta_c,theta_m,I_t);
-        fwrite(buffer, sizeof(int), B_SIZE, stdout);
+        p1 += f_c*2*M_PI/S_RATE+theta_c;
+        p2 += f_m*2*M_PI/S_RATE+theta_m;
+        I_t += 0.0001*sign;
+        if(I_t>100||I_t<0) sign=-sign;
+
+        buffer[index]=(int) (amplitude*(cos(p1+I_t*(cos(p2)))));
+
+        index++;
+        if(index%B_SIZE==0) {
+            fprintf(stderr,"f_c=%02.2f f_m=%02.2f theta_c=%02.2f theta_m=%02.2f I_t=%02.2f\r",f_c,f_m,theta_c,theta_m,I_t);
+            fwrite(buffer, sizeof(int), B_SIZE, stdout);
+            index=0;
+        }
     }
+    endwin();
 }
