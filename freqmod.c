c8171cafe14f085ea3e4cd3cfa0d1f2017db7d76
Colin <colin.diesh@gmail.com>
2015-11-28 14:55:33 -0600

    Fix the timing constant

--- freqmod.c
+++ freqmod.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdio.h>
+#include <stdlib.h>
 #include <ncurses.h>
 #include <stdint.h>
 
@@ -13,12 +13,11 @@
 //I(t) modulation index
 //fc the carrier frequency
 //fm the modulating frequency
-//osc2_t,c phase constants
+//osc1_t,osc2_t phase constants
 
 
 int main() {
     uint16_t buffer[B_SIZE];
-    float p1=0,p2=0,p3=0;
     float osc1=40,osc2=80,osc3=40;
     float A=50;
     float osc2_t=0,osc1_t=0,osc3_t=0;
@@ -43,56 +42,56 @@ int main() {
         else if(ch=='x') { osc3/=2; }
         else if(ch=='d') { osc2_t+=.01; }
         else if(ch=='f') { osc2_t-=.01; }
-        else if(ch=='c') { osc1_t+=.01; }
-        else if(ch=='v') { osc1_t-=.01; }
-        else if(ch=='e') { osc3_t+=.01; }
-        else if(ch=='r') { osc3_t-=.01; }
-        else if(ch=='b') { osc1_type++;osc1_type%=3; }
+        else if(ch=='c') { osc3_t+=.01; }
+        else if(ch=='v') { osc3_t-=.01; }
+        else if(ch=='e') { osc1_t+=.01; }
+        else if(ch=='r') { osc1_t-=.01; }
+        else if(ch=='t') { osc1_type++;osc1_type%=3; }
         else if(ch=='g') { osc2_type++;osc2_type%=3; }
-        else if(ch=='t') { osc3_type++;osc3_type%=3; }
+        else if(ch=='b') { osc3_type++;osc3_type%=3; }
         else if(ch=='m') { third_mod++;third_mod%=3; }
 
 
-        t=1/S_RATE;
-        p1 += osc1*2*M_PI/S_RATE+osc1_t;
-        p2 += osc2*2*M_PI/S_RATE+osc2_t;
-        p3 += osc3*2*M_PI/S_RATE+osc3_t;
+        t += 1.0/S_RATE;
+        float p1 = osc1*2*M_PI*t+osc1_t;
+        float p2 = osc2*2*M_PI*t+osc2_t;
+        float p3 = osc3*2*M_PI*t+osc3_t;
 
         I_t += 0.0001*sign;
         if(I_t>100||I_t<0) sign=-sign;
 
         if(third_mod==0) {
-            float c1=cos(p2+osc2_t);
-            if(osc2_type==1) c1 = c1<=0?-1:1;
-            if(osc2_type==2) c1 = osc2 * (1/osc2 - abs(index % (int) (osc2/2) - 1/osc2) );
-            float c2=cos(p1+I_t*c1+osc2_t);
-            if(osc1_type==1) c2 = c2<=0?-1:1;
-            if(osc1_type==2) c2 = osc1 * (1/osc1 - abs(index % (int) (osc1/2) - 1/osc1) );
+            float c1=cos(p1+osc1_t);
+            if(osc1_type==1) c1 = c1<=0?-1:1;
+            if(osc1_type==2) c1 = 1/osc1 * (osc1 - abs(index % (int) (osc1*2) - osc1) );
+            float c2=cos(p2+I_t*c1+osc2_t);
+            if(osc2_type==1) c2 = c2<=0?-1:1;
+            if(osc2_type==2) c2 = 1/osc2 * (osc2 - abs(index % (int) (osc2*2) - osc2) );
             buffer[index]=A*c2;
         }
 
         else if(third_mod==1) {
             float c1=cos(p2);
             if(osc2_type==1) c1 = c1<=0?-1:1;
-            if(osc2_type==2) c1 = osc2 * (1/osc2 - abs(index % (int) (osc2/2) - 1/osc2) );
+            if(osc2_type==2) c1 = 1/osc2 * (osc2 - abs(index % (int) (osc2*2) - osc2) );
             float c2=cos(p1+I_t*c1);
             if(osc1_type==1) c2 = c2<=0?-1:1;
-            if(osc1_type==2) c2 = osc1 * (1/osc1 - abs(index % (int) (osc1/2) - 1/osc1) );
+            if(osc1_type==2) c2 = 1/osc1 * (osc1 - abs(index % (int) (osc1*2) - osc1) );
             float c3=cos(p3+I_t*c2);
             if(osc3_type==1) c3 = c3<=0?-1:1;
-            if(osc3_type==2) c3 = osc3 * (1/osc3 - abs(index % (int) (osc3/2) - 1/osc3) );
+            if(osc3_type==2) c3 = 1/osc3 * (osc3 - abs(index % (int) (osc3*2) - osc3) );
             buffer[index]=A*c3;
         }
         else if(third_mod==2) {
             float c1=cos(p2);
             if(osc2_type==1) c1 = c1<=0?-1:1;
-            if(osc2_type==2) c1 = osc2 * (1/osc2 - abs(index % (int) (osc2/2) - 1/osc2) );
+            if(osc2_type==2) c1 = 1/osc2 * (osc2 - abs(index % (int) (osc2*2) - osc2) );
             float c2=cos(p1);
             if(osc1_type==1) c2 = c2<=0?-1:1;
-            if(osc1_type==2) c2 = osc1 * (1/osc1 - abs(index % (int) (osc1/2) - 1/osc1) );
+            if(osc1_type==2) c2 = 1/osc1 * (osc1 - abs(index % (int) (osc1*2) - osc1) );
             float c3=cos(p3+I_t*c2+I_t*c1);
             if(osc3_type==1) c3 = c3<=0?-1:1;
-            if(osc3_type==2) c3 = osc3 * (1/osc3 - abs(index % (int) (osc3/2) - 1/osc3) );
+            if(osc3_type==2) c3 = 1/osc3 * (osc3 - abs(index % (int) (osc3*2) - osc3) );
             buffer[index]=A*c3;
         }
 
@@ -102,7 +101,7 @@ int main() {
             if(third_mod>0) {
                 fprintf(stderr,"osc1=%02.2f/%d\tosc2=%02.2f/%d\tosc3=%02.2f/%d\tt_c=%02.2f\tt_m=%02.2f\tt_ck=%02.2f\tI_t=%02.2f\t%s\r\n",
                     osc1,osc1_type,osc2,osc2_type,osc3,osc3_type,osc1_t,osc2_t,osc3_t,I_t,
-                    third_mod==0?"osc2->osc1":third_mod==1?"osc1->osc2->osc3":"osc1->osc2<-osc3");
+                    third_mod==0?"osc1->osc2":third_mod==1?"osc1->osc2->osc3":"osc1->osc2<-osc3");
             }
             else {
                 fprintf(stderr,"osc1=%02.2f/%d\tosc2=%02.2f/%d\tt_c=%02.2f\tt_m=%02.2f\tI_t=%02.2f\t%s\r\n",
